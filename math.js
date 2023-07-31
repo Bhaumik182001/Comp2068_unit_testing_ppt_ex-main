59c038bf8ffd1a30a11edd5a87d07a1d2430e6b5
Bhaumik182001 <bhaumik182001@gmail.com>
2023-07-30 20:46:11 -0400

    first commit

---
+++ math.js
@@ -0,0 +1,11 @@
+// math.js
+// This file contains a simple function that will be tested by test/math.test.js file 
+
+// function for adding two numbers
+function add(a, b) {
+  return a + b;
+}
+
+//EXERCISE: write a function to multiply two numbers
+
+module.exports = add;
